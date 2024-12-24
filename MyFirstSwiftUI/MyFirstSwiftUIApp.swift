9d62f2adf6dd21a98c93ae553060c177d62ea796
spark-o-petit <petitspark247@gmail.com>
2024-12-25 01:04:04 +0900

    Initial Commit

---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by alex on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyFirstSwiftUITests/MyFirstSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUITests.swift
+//  MyFirstSwiftUITests
+//
+//  Created by alex on 12/25/24.
+//
+
+import Testing
+@testable import MyFirstSwiftUI
+
+struct MyFirstSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
