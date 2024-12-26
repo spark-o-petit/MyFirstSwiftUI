3ebd5c8782ef3e9e485f0fdc06cd762a629cca59
spark-o-petit <petitspark247@gmail.com>
2024-12-27 00:57:58 +0900

    Enhance reusability with SwiftUI practice template

---
+++ MyFirstSwiftUI/UI-template.swift
@@ -0,0 +1,19 @@
+//
+//  Untitled.swift
+//  MyFirstSwiftUI
+//
+//  Created by alex on 12/27/24.
+//
+
+import SwiftUI
+
+struct MyToggle {
+  var body: some View {
+    
+  }
+  
+}
+
+#Preview {
+  MyToggle()
+}
